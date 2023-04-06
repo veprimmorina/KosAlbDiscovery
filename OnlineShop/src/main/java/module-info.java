41e7272a3f653fca28c5d784274b2b3982b716f6
veprimmorina <65460301+veprimmorina@users.noreply.github.com>
2023-04-06 14:17:28 +0200

    Changes

--- OnlineShop/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.onlineshop {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.onlineshop to javafx.fxml;
-    exports com.example.onlineshop;
-}
